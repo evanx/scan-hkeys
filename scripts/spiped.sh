054751117deb82c8ddb690ce2d524a5d90d9798d
Evan Summers <evan.summers@gmail.com>
2017-01-24 22:45:44 +0000

    initial

--- scripts/spiped.sh
+++ scripts/spiped.sh
@@ -1,21 +1,30 @@
 (
   set -u -e -x
-  dd if=/dev/urandom bs=32 count=1 > ~/tmp/test-redis-spiped-keyfile
+  mkdir -p tmp
+  dd if=/dev/urandom bs=32 count=1 > tmp/test-spiped-keyfile
   docker network create -d bridge test-hkeys-network
-  container=`docker run --network=test-hkeys-network \
-    --name test-redis-hkeys -d tutum/redis`
-  spipedEncryptingContainer=`docker run --network=test-hkeys-network \
-      --name test-docker run -d -v ~/tmp/test-redis-spiped-keyfile:/spiped/key:ro \
-      --network=host -p 6334:6334 spiped \
-      -d -s "[0.0.0.0]:6334" -t "[127.0.0.1]:6379"`
-  password=`docker logs $container | grep '^\s*redis-cli -a' |
-    sed -e 's/^\s*redis-cli -a \(\w*\) .*$/\1/'`
-  redisHost=`docker inspect $container |
+  redisContainer=`docker run --network=test-hkeys-network \
+      --name test-hkeys-redis -d tutum/redis`
+  password=`docker logs $redisContainer | grep '^\s*redis-cli -a' |
+      sed -e 's/^\s*redis-cli -a \(\w*\) .*$/\1/'`
+  redisHost=`docker inspect $redisContainer |
+      grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
+  decipherContainer=`docker run --network=test-hkeys-network \
+    --name test-decipher -d -v tmp/test-spiped-keyfile:/spiped/key:ro \
+    -p 6444:6444 spiped \
+    -d -s "[0.0.0.0]:6444" -t "[$redisHost]:6379"`
+  decipherHost=`docker inspect $decipherContainer |
     grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
-  redis-cli -a $password -h $redisHost hset mytest:64:h name 'Pottery Place'
-  redis-cli -a $password -h $redisHost hset mytest:64:h address '48 High Street'
+  encipherContainer=`docker run --network=test-hkeys-network \
+    --name test-encipher -e -v tmp/test-spiped-keyfile:/spiped/key:ro \
+    -p 6379:6379 spiped \
+    -d -s "[0.0.0.0]:6379" -t "[$decipherHost]:6444"`
+  encipherHost=`docker inspect $encipherContainer |
+    grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
+  redis-cli -a $password -h $encipherHost hset mytest:64:h name 'Pottery Place'
+  redis-cli -a $password -h $encipherHost hset mytest:64:h address '48 High Street'
   docker run --network=test-hkeys-network \
-    -e host=$redisHost -e password=$password \
+    -e host=$encipherHost -e password=$password \
     -e pattern=mytest:*:h evanxsummers/scan-hkeys
   docker rm -f `docker ps -q -f name=test-redis-hkeys`
   docker network rm test-hkeys-network
